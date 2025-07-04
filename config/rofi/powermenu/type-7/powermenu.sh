88578041ba68c825e2e1674d6112952d51a882cd
marcorgz1 <marco.valdemoro@gmail.com>
2025-07-04 15:48:06 +0200

    feat: update some rofi menus to improve the design and match it with the new color scheme

--- config/rofi/powermenu/type-7/powermenu.sh
+++ config/rofi/powermenu/type-7/powermenu.sh
@@ -1,4 +1,4 @@
-CHOICE=$(printf "\n\n\n\n" | rofi -dmenu -replace -config ~/.config/rofi/powermenu/type-7/style-1.rasi)
+CHOICE=$(printf "\n\n\n\n" | rofi -dmenu -replace -config ~/.config/rofi/powermenu/type-7/style-1.rasi)
 
 case "$CHOICE" in
   "")
